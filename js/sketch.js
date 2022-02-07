4b1419645773ceae0f43cc8907a6c69b456230a1
Retky <jose.arredondo.a@outlook.com>
2022-02-06 20:06:34 -0600

    "createGrid" set grid format

--- js/sketch.js
+++ js/sketch.js
@@ -3,11 +3,16 @@ const grid = document.getElementById('grid')
 
 // THIS f create the grid with the box element.
 function createGrid(elements) {
-    for (let i = 0; i < elements; i++) {
-        console.log('count')
+    let gridArray = []
+    for (let i = 1; i <= (elements * elements); i++) {
         // THIS create each div.
         box = document.createElement('div');
         grid.appendChild(box)
+        if (i <= elements) {
+            gridArray.push(i)
+        }
     }
+    //  THIS set the grid colums.
+    grid.style.setProperty('grid-template-areas', ("'" + gridArray.join(' ') + "'"))
 }
 createGrid(16)
