36c038f4219123c05f77a60781e4003f4a8a8810
Retky <jose.arredondo.a@outlook.com>
2022-02-06 21:56:50 -0600

    "box" event change backgroudn to black

--- js/sketch.js
+++ js/sketch.js
@@ -7,6 +7,14 @@ function createGrid(elements) {
     for (let i = 1; i <= (elements * elements); i++) {
         // THIS create each div.
         box = document.createElement('div');
+        let itemName = 'box' + i;
+        box.setAttribute('id', itemName)
+        box.setAttribute('class', 'box')
+        // THIS is the action on event (change color to black).
+        box.addEventListener('mouseover', function () {
+            this.style.background = 'black'
+        })
+        // THIS log the value of grid columns.
         grid.appendChild(box)
         if (i <= elements) {
             gridArray.push(i)
